8952ca0e2086cd16e18122ef6bc68b296220f97b
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2025-01-20 14:25:56 +0000

    something

---
+++ online_round2/problem1/solution.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+# # Load the CSV file
+# file_path = input()
+data = pd.read_csv("/workspaces/IITK-Hackathon/online_round2/problem1/file1.csv")
+
+patterns = ["'", "--", ";", "SELECT", "UNION", "INSERT", "UPDATE", "DELETE"]
+pattern = '|'.join(patterns)
+attempts = data[data["Info"].str.contains(pattern, case=False, na=False)]
+
+if not attempts.empty:
+    source_ip = attempts["Source"].iloc[0]
+    print(f"1A:{source_ip};")
+else:
+    print("1A:NULL;")
+
+total_attempts = len(attempts)
+print(f"2A:{total_attempts};")
+
+if not attempts.empty:
+    first_payload = attempts.sort_values(by="Time").iloc[0]["Info"]
+    print(f"3A:{first_payload};")
+else:
+    print("3A:NULL;")
+
+if not attempts.empty:
+    last_payload = attempts.sort_values(by="Time").iloc[-1]["Info"]
+    print(f"4A:{last_payload};")
+else:
+    print("4A:NULL;")
+
+colons = attempts[attempts["Info"].str.contains(":", case=False, na=False)]
+total_colons = len(colons)
+print(f"5A:{total_colons};")
