8fa5127d30b8fb36ac190b8287649b1531984e80
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2025-01-20 15:04:43 +0000

    fixed output pattern

--- online_round2/problem1/solution.py
+++ online_round2/problem1/solution.py
@@ -10,25 +10,25 @@ attempts = data[data["Info"].str.contains(pattern, case=False, na=False)]
 
 if not attempts.empty:
     source_ip = attempts["Source"].iloc[0]
-    print(f"1A:{source_ip};")
+    print(f"1A:-{source_ip}-;")
 else:
-    print("1A:NULL;")
+    print("1A:-NULL-;")
 
 total_attempts = len(attempts)
-print(f"2A:{total_attempts};")
+print(f"2A:-{total_attempts}-;")
 
 if not attempts.empty:
     first_payload = attempts.sort_values(by="Time").iloc[0]["Info"]
-    print(f"3A:{first_payload};")
+    print(f"3A:-{first_payload}-;")
 else:
-    print("3A:NULL;")
+    print("3A:-NULL-;")
 
 if not attempts.empty:
     last_payload = attempts.sort_values(by="Time").iloc[-1]["Info"]
-    print(f"4A:{last_payload};")
+    print(f"4A:-{last_payload}-;")
 else:
-    print("4A:NULL;")
+    print("4A:-NULL-;")
 
 colons = attempts[attempts["Info"].str.contains(":", case=False, na=False)]
 total_colons = len(colons)
-print(f"5A:{total_colons};")
+print(f"5A:-{total_colons}-;")
