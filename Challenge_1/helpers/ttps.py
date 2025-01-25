993b698c769de0659b1da11643c1cd0363a81053
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2025-01-25 16:46:26 +0000

    fixed the ttps i guess

--- Challenge_1/helpers/ttps.py
+++ Challenge_1/helpers/ttps.py
@@ -1,26 +1,35 @@
+import spacy
+import re
+import requests
+
+response = requests.get('https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json')
+attack_data = response.json()
+
 class Ttps:
-    def __init__(self, text):
+    def __init__(self, text, nlp):
         self.text = text
+        self.nlp = nlp
 
     def get_info(self):
-        known_tactics = {
-            "Initial Access": "TA0001",
-            "Execution": "TA0002",
-            "Persistence": "TA0003",
-            "Privilege Escalation": "TA0004",
-            "Defense Evasion": "TA0005",
-            "Credential Access": "TA0006",
-            "Discovery": "TA0007",
-            "Lateral Movement": "TA0008",
-            "Collection": "TA0009",
-            "Exfiltration": "TA0010",
-            "Command and Control": "TA0011",
-            "Impact": "TA0040",
-            "Resource Development": "TA0042",
-            "Reconnaissance": "TA0043",
+        techniques = []
+        tactics = []
+
+        for obj in attack_data.get('objects', []):
+            if obj.get('type') == 'attack-pattern':
+                techniques.append((obj['name'], obj['external_references'][0]['external_id']))
+            elif obj.get('type') == 'x-mitre-tactic':
+                tactics.append((obj['name'], obj['external_references'][0]['external_id']))
+
+        text = re.sub('[^A-Za-z\s]+', '', self.text)
+        text = text.lower()
+
+        doc = self.nlp(text)
+        keywords = [token.text for token in doc if token.is_alpha and not token.is_stop]
+
+        matched_techniques = [(tech, code) for tech, code in techniques if any(kw in tech.lower() for kw in keywords)]
+        matched_tactics = [(tact, code) for tact, code in tactics if any(kw in tact.lower() for kw in keywords)]
+    
+        return {
+            "Tactics": {tactic[1]: tactic[0] for tactic in matched_tactics},
+            "Techniques": {technique[1]: technique[0] for technique in matched_techniques}
         }
-        ttps = []
-        for tactic in known_tactics:
-            if tactic in self.text:
-                ttps.append({known_tactics[tactic]: tactic})
-        return ttps
--- Challenge_1/solution.py
+++ Challenge_1/solution.py
@@ -12,58 +12,58 @@ from helpers.targeted_entities import TargetedEntities
 nlp = spacy.load("en_core_web_sm")
 
 def read_file(filename):
-  if ".txt" in filename:
-    with open(filename, 'r') as file:
-      return file.read()
-  elif ".pdf" in filename:
-    file = pdf.open(filename)
-    text = ""
-    for page in file:
-      text += str(page.get_text())
-    return text
+    if ".txt" in filename:
+        with open(filename, 'r') as file:
+            return file.read()
+    elif ".pdf" in filename:
+        file = pdf.open(filename)
+        text = ""
+        for page in file:
+            text += str(page.get_text())
+        return text
 
 def display(filename, data, out_dir, show):
-  if not out_dir and show:
-    print(filename)
-  if out_dir:
-    with open(f"{out_dir}/{filename.split('.')[0]}.json", 'w') as file:
-      file.write(data)
-  else:
-    print(data)
+    if not out_dir and show:
+        print(filename)
+    if out_dir:
+        with open(f"{out_dir}/{filename.split('.')[0]}.json", 'w') as file:
+            file.write(data)
+    else:
+        print(data)
 
 def start(report_text):
-  iocs = Iocs(report_text)
-  ttps = Ttps(report_text)
-  threat_actors = ThreatActors(report_text, nlp)
-  malware = Malware(report_text)
-  targeted_entities = TargetedEntities(report_text, nlp)
+    iocs = Iocs(report_text)
+    ttps = Ttps(report_text, nlp)
+    threat_actors = ThreatActors(report_text, nlp)
+    malware = Malware(report_text)
+    targeted_entities = TargetedEntities(report_text, nlp)
 
-  data = {
-    "IoCs": iocs.get_info(),
-    "TTPs": ttps.get_info(),
-    "Threat Actor(s)": threat_actors.get_info(),
-    "Malware": malware.get_info(),
-    "Targeted Entities": targeted_entities.get_info(),
-  }
-  return json.dumps(data, indent=4)
+    data = {
+        "IoCs": iocs.get_info(),
+        "TTPs": ttps.get_info(),
+        "Threat Actor(s)": threat_actors.get_info(),
+        "Malware": malware.get_info(),
+        "Targeted Entities": targeted_entities.get_info(),
+    }
+    return json.dumps(data, indent=4)
   
 if __name__=="__main__":
-  parser = argparse.ArgumentParser(
-      prog="Solution",
-      description="Extracts Key Threat Intelligence Data From Given File, including Indicators of Compromise, TTTps, Malware Information, Threat Actors and Targeted Entities"
-  )
-  parser.add_argument("-f", "--file", help = "Filename to process, can be .pdf or .txt only")
-  parser.add_argument("-d", "--dir", help = "Directory to search files in, will not search recursively")
-  parser.add_argument("-o", "--out", help = "Directory to store output .json file, output filename is same as input filename")
-  parser.add_argument("-s", "--show", help = "Prints filename to stdout", action="store_true")
-  args = parser.parse_args()
-  arg = vars(args)
+    parser = argparse.ArgumentParser(
+        prog="Solution",
+        description="Extracts Key Threat Intelligence Data From Given File, including Indicators of Compromise, TTTps, Malware Information, Threat Actors and Targeted Entities"
+    )
+    parser.add_argument("-f", "--file", help = "Filename to process, can be .pdf or .txt only")
+    parser.add_argument("-d", "--dir", help = "Directory to search files in, will not search recursively")
+    parser.add_argument("-o", "--out", help = "Directory to store output .json file, output filename is same as input filename")
+    parser.add_argument("-s", "--show", help = "Prints filename to stdout", action="store_true")
+    args = parser.parse_args()
+    arg = vars(args)
 
-  if not arg['dir'] and not arg['file']:
-    parser.parse_args(['-h'])
-  if arg["dir"]:
-    files = listdir(arg["dir"])
-    for file in files:
-      display(file, start(read_file(f"{arg['dir']}/{file}")), arg["out"], arg["show"])
-  if arg["file"]:
-    display(arg["file"], start(read_file(arg["file"])), arg["out"], arg["show"])
+    if not arg['dir'] and not arg['file']:
+        parser.parse_args(['-h'])
+    if arg["dir"]:
+        files = listdir(arg["dir"])
+        for file in files:
+            display(file, start(read_file(f"{arg['dir']}/{file}")), arg["out"], arg["show"])
+    if arg["file"]:
+        display(arg["file"], start(read_file(arg["file"])), arg["out"], arg["show"])
