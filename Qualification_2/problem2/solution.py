647c2d5e2a99275388dc1d95f7b6a31016522bd2
EM <greyhatem@gmail.com>
2025-01-25 10:29:19 +0530

    Fix

--- Qualification_2/problem2/solution.py
+++ Qualification_2/problem2/solution.py
@@ -1,4 +1,5 @@
 from hashlib import sha256
+from binascii import hexlify
 
 hashes = []
 
@@ -11,12 +12,16 @@ def merkel(num):
         num += 1
     for i in range(0, num, 2):
         concat = hashes[i] + hashes[i+1]
-        hashes[i//2] = sha256(sha256(concat))
-    merkel(num//2)
+        concat = hexlify(concat.encode())
+        hash = sha256(concat).hexdigest()
+        hashes[i//2] = hash
+    return merkel(num//2)
 
 if __name__ == "__main__":
     for _ in range(0, int(input())):
         strings = int(input())
         for _ in range(0, strings):
-            hashes.append(input().encode())
-            print(merkel(len(hashes)))
+            hash = sha256(input().encode()).hexdigest()
+            hashes.append(hash)
+        hashes.append("")
+        print(merkel(strings))
