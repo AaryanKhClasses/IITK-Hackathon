27dc3c7791535be0e5d924135a5c4120ecc878a6
EM <greyhatem@gmail.com>
2025-01-25 16:32:53 +0530

    Added help message

--- Challenge_1/solution.py
+++ Challenge_1/solution.py
@@ -1,5 +1,6 @@
 import spacy
 import json
+import argparse
 from helpers.iocs import Iocs
 from helpers.ttps import Ttps
 from helpers.malware_detect import Malware
@@ -8,24 +9,53 @@ from helpers.targeted_entities import TargetedEntities
 
 nlp = spacy.load("en_core_web_sm")
 
-report_text = ''' 
-The APT33 group, suspected to be from Iran, has launched a new campaign targeting the energy sector organizations. 
-The attack utilizes Shamoon malware, known for its destructive capabilities. The threat actor exploited a vulnerability in the network perimeter to gain initial access. 
-The malware was delivered via spear-phishing emails containing a malicious attachment. The malware's behavior was observed communicating with IP address 192.168.1.1 and domain example.com. The attack also involved lateral movement using PowerShell scripts. 
-''' 
+def read_file(filename):
+  if ".txt" in filename:
+    with open(filename, 'r') as file:
+      return file.read()
+  elif ".pdf" in filename:
+    pass
 
-iocs = Iocs(report_text)
-ttps = Ttps(report_text)
-threat_actors = ThreatActors(report_text, nlp)
-malware = Malware(report_text)
-targeted_entities = TargetedEntities(report_text, nlp)
+def display(filename, data, out_dir, show):
+  if not out_dir and show:
+    print(filename)
+  if out_dir:
+    with open(f"{out_dir}/{filename.split('.')[0]}.json", 'w') as file:
+      file.write(data)
+  else:
+    print(data)
 
-data = {
+def start(report_text):
+  iocs = Iocs(report_text)
+  ttps = Ttps(report_text)
+  threat_actors = ThreatActors(report_text, nlp)
+  malware = Malware(report_text)
+  targeted_entities = TargetedEntities(report_text, nlp)
+
+  data = {
     "IoCs": iocs.get_info(),
     "TTPs": ttps.get_info(),
     "Threat Actor(s)": threat_actors.get_info(),
     "Malware": malware.get_info(),
     "Targeted Entities": targeted_entities.get_info(),
-}
+  }
+  return json.dumps(data, indent=4)
+  
+if __name__=="__main__":
+  parser = argparse.ArgumentParser(
+      prog="Solution",
+      description="Extracts Key Threat Intelligence Data From Given File, including Indicators of Compromise, TTTps, Malware Information, Threat Actors and Targeted Entities"
+  )
+  parser.add_argument("-f", "--file", help = "Filename to process, can be .pdf or .txt only")
+  parser.add_argument("-d", "--dir", help = "Directory to search files in")
+  parser.add_argument("-o", "--out", help = "Directory to store output .json file, output filename is same as input filename")
+  parser.add_argument("-s", "--show", help = "Prints filename to stdout", action="store_true")
+  args = parser.parse_args()
+  arg = vars(args)
 
-print(json.dumps(data, indent=4))
+  if not arg['dir'] and not arg['file']:
+    parser.parse_args(['-h'])
+  if arg["dir"]:
+    dir = arg["dir"]
+  if arg["file"]:
+    display(arg["file"], start(read_file(arg["file"])), arg["out"], arg["show"])
