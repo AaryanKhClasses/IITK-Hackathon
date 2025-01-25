998eebfcd919b3c8cc8485adbc95270633260113
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2025-01-25 09:43:35 +0000

    fixed targetting entities and threat anchors.... i guess...

--- Challenge_1/helpers/targeted_entities.py
+++ Challenge_1/helpers/targeted_entities.py
@@ -1,11 +1,22 @@
+from .threat_actors import ThreatActors
+
 class TargetedEntities:
     def __init__(self, text, nlp):
         self.text = text
         self.nlp = nlp
 
     def get_info(self):
+        unwanted_terms = ["IP", "PowerShell"]
+        threat_actors = ThreatActors(self.text, self.nlp).get_info()
         entities = []
         for ent in self.nlp(self.text).ents:
-            if ent.label_ in ["ORG", "GPE"]:
+            if ent.label_ in ["ORG", "GPE"] and ent.text not in threat_actors and ent.text not in unwanted_terms:
                 entities.append(ent.text)
+        
+        # Dynamically include specific sectors
+        sector_keywords = ["sector", "organization", "industry"]
+        for chunk in self.nlp(self.text).noun_chunks:
+            if any(keyword in chunk.text.lower() for keyword in sector_keywords):
+                entities.append(chunk.text)
+        
         return entities
--- Challenge_1/helpers/threat_actors.py
+++ Challenge_1/helpers/threat_actors.py
@@ -1,11 +1,17 @@
+import re
+
 class ThreatActors:
     def __init__(self, text, nlp):
         self.text = text
         self.nlp = nlp
 
     def get_info(self):
+        unwanted_terms = ["IP", "PowerShell"]
         threat_actors = []
         for ent in self.nlp(self.text).ents:
-            if ent.label_ == "ORG":
+            if ent.label_ in ["ORG", "GPE", "NORP"] and ent.text not in unwanted_terms:
                 threat_actors.append(ent.text)
+        pattern = re.compile(r'\bAPT\d+\b', re.IGNORECASE)
+        threat_actors.extend(pattern.findall(self.text))
+        
         return threat_actors
